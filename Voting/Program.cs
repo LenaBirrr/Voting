b44fbe5d9ff3b7fc53a7a1822bb0d4cfa454f630
LenaBirrr <lenabir68@gmail.com>
2022-09-29 20:34:57 +0300

    Добавьте файлы проекта.

---
+++ Voting/Program.cs
@@ -0,0 +1,8 @@
+﻿Voting.Voting voting = new Voting.Voting();
+string votersAddress = "test_address";
+Voting.Voting.Choice votersChoice = Voting.Voting.Choice.CANDIDAD_1;
+voting.ValidateVoter(votersAddress);
+voting.ValidateChoice(ref votersChoice, false);
+Console.WriteLine(voting.GetWinner().ToString());
+
+
---
+++ Voting/Voting.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voting
+{
+    public class Voting
+    {
+        public enum Choice
+        {
+            NONE,
+            CANDIDAD_1,
+            CANDIDAT_2,
+            CANDIDAT_3,
+            CANDIDAT_MULTIPAL,
+        }
+        public string address = "test_address";
+        public int[] votes = new int[3];
+        public void ValidateChoice(ref Choice choice, bool spoiled)
+        {
+            if(choice==Choice.NONE||choice==Choice.CANDIDAT_MULTIPAL)
+            {
+                spoiled = true;
+            }
+            else
+            {
+                votes[((int)choice)]++;
+            }
+        }
+
+        public bool ValidateVoter(string address)
+        {
+            return this.address == address;
+        }
+        public Choice GetWinner()
+        {
+            return (Choice)Array.IndexOf(votes,votes.Max());
+        }
+
+    }
+}
